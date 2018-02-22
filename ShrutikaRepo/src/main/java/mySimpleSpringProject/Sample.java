ccf44e27b3438ad5055db337878915122b04dde4
SYadav3 <shrutika.yadav@tcs.com>
2018-02-22 05:17:26 -0800

    let's see!

--- ShrutikaRepo/src/main/java/mySimpleSpringProject/Sample.java
+++ ShrutikaRepo/src/main/java/mySimpleSpringProject/Sample.java
@@ -3,8 +3,6 @@ class Sample{
   public String returnSample(){
 	  String a ="QWERTY2!";
 	  String abc="fuhruht";
-	  String p = "fghsdgf";
-	  String c = "Shrutika yadav"
 	  int a = 6;
     return "This is a sample class";
     
