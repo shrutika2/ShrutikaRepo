a9f4383c026a7a6820cbea6822f24bf07f21d988
shrutika2 <36729361+shrutika2@users.noreply.github.com>
2018-02-22 05:16:25 -0800

    Update Sample.java
    all variables removed!

--- ShrutikaRepo/src/main/java/mySimpleSpringProject/Sample.java
+++ ShrutikaRepo/src/main/java/mySimpleSpringProject/Sample.java
@@ -1,11 +1,6 @@
 package mySimpleSpringProject;
 class Sample{
   public String returnSample(){
-	  String a ="QWERTY2!";
-	  String abc="fuhruht";
-	  String p = "fghsdgf";
-	  String c = "Shrutika yadav"
-	  int a = 6;
     return "This is a sample class";
     
   }
