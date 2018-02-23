4e629a7d8fc003cb963dbe0445206513c21f6d36
shrutika2 <36729361+shrutika2@users.noreply.github.com>
2018-02-22 23:53:12 -0800

    Update Sample.java

--- ShrutikaRepo/src/main/java/mySimpleSpringProject/Sample.java
+++ ShrutikaRepo/src/main/java/mySimpleSpringProject/Sample.java
@@ -1,12 +1,6 @@
 package mySimpleSpringProject;
 class Sample{
   public String returnSample(){
-<<<<<<< HEAD
-	  String a ="QWERTY2!";
-	  String abc="fuhruht";
-	  int a = 6;
-=======
->>>>>>> a9f4383c026a7a6820cbea6822f24bf07f21d988
     return "This is a sample class";
     
   }
